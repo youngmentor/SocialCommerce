f7d83516c12856d87cb52b3cf5a27ff4d9d25d34
Oriade, Yusuf Olamilekan <crownhorla07@gmail.com>
2023-10-19 08:47:06 +0100

    Building The Login Page

--- social-commerce/src/Component1/Onboarding/Login/Login.tsx
+++ social-commerce/src/Component1/Onboarding/Login/Login.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "./Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <div className="main">
+        <div className="mainWrap">
+          <div className="LoginText">
+            <h1>Social  Commerce</h1>
+            <span>Social-Commerce helps you connect and sell your products with the people in your life</span>
+          </div>
+          <div className="LoginInput">
+            <div className="LoginInputBox">
+              <div className="LoginInputBoxWrap">
+                <input type="text" id='contact' placeholder='Email address or phone number' />
+                <input type="password" placeholder='Password' />
+                <button>Log in</button>
+                <p>Forgotten password?</p>
+                <hr className='grayLine'/>
+                <button className='SignUpButton'>Create new  account</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
