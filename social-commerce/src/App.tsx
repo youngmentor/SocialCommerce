3f48119ebe562072bcc800a90f769470bfcebdfe
Kennedy2042 <ogbonnakenedy@gmail.com>
2023-10-19 09:25:00 +0100

    ReactRouterDom Added and path for login and signup add successfully

--- social-commerce/src/App.tsx
+++ social-commerce/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './Component1/Onboarding/Signup/Signup';
 import Login from './Component1/Onboarding/Login/Login';
@@ -6,8 +7,12 @@ import Login from './Component1/Onboarding/Login/Login';
 function App() {
   return (
     <div className="App">
-        <Signup/>
-        <Login />
+      <HashRouter>
+        <Routes>
+        <Route path='/login' element={<Login />}/>
+          <Route path='/' element={<Signup/>}/>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
