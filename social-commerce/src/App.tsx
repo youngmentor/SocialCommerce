25e886ce71d95ca74f51958d2c655cc2f2add4f7
youngmentor <olaleresuliton43@gmail.com>
2023-10-19 18:04:24 +0100

    added forget password page and also created schema.ts file and type.check.ts file and also installed react-hook-form and yup and also yupResolver for validating my form

--- social-commerce/src/App.tsx
+++ social-commerce/src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './Component1/Onboarding/Signup/Signup';
 import Login from './Component1/Onboarding/Login/Login';
+import ForgetPassword from './Component1/Onboarding/ForgetPassword/ForgetPassword';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <HashRouter>
         <Routes>
         <Route path='/login' element={<Login />}/>
-          <Route path='/' element={<Signup/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path="/" element={<ForgetPassword/>}/>
         </Routes>
       </HashRouter>
     </div>
