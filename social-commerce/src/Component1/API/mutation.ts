b407e3a1075fdf8e295bc59df17676b65437acac
youngmentor <olaleresuliton43@gmail.com>
2023-10-22 07:17:43 +0100

    added new changes to the forget password page and also installed react-query and axios

---
+++ social-commerce/src/Component1/API/mutation.ts
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+export const login = async ()=>{
+    return await axios.post(``)
+}
+
+export const forgetPassword = async ()=>{
+    return await axios.post(``)
+}
+
+export const signup = async ()=>{
+    return await axios.post(``)
+}
